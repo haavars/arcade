69e0e215f0dc9205174098af6a563b8bb72e083f
Håvar Stavseth <haavar.stavseth@gmail.com>
2021-12-18 21:47:23 +0100

    Working form

--- lib/arcade/invite.ex
+++ lib/arcade/invite.ex
@@ -1,12 +1,12 @@
-# defmodule Arcade.Invite do
-#   alias Arcade.Invite.Recipient
+defmodule Arcade.Invite do
+  alias Arcade.Invite.Recipient
 
-#   def change_invitation(%Recipient{} = recipient, attrs \\ %{}) do
-#     Recipient.changeset(recipient, attrs)
-#   end
+  def change_invitation(%Recipient{} = recipient, attrs \\ %{}) do
+    Recipient.changeset(recipient, attrs)
+  end
 
 
-#   def send_invite(recipient, attrs) do
-#     # send email to promo recipient
-#   end
-# end
+  def send_invite(recipient, attrs) do
+    # send email to promo recipient
+  end
+end
--- lib/arcade_web/live/invite_live.ex
+++ lib/arcade_web/live/invite_live.ex
@@ -1,11 +1,24 @@
 defmodule ArcadeWeb.InviteLive do
   use ArcadeWeb, :live_view
-
   alias Arcade.Invite
-
   alias Arcade.Invite.Recipient
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+      socket
+      |> assign_recipient()
+      |> assign_changeset()}
+  end
+
+
+  def assign_recipient(socket) do
+    socket
+    |> assign(:recipient, %Recipient{})
+  end
+
+
+  def assign_changeset(%{assigns: %{recipient: recipient}} = socket) do
+    socket
+    |> assign(:changeset, Invite.change_invitation(recipient))
   end
 end
