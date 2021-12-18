068b3d8c7a7f4da0fc74a229ba9304c0e151cf06
Håvar Stavseth <haavar.stavseth@gmail.com>
2021-12-18 21:52:53 +0100

    added validation handler

--- lib/arcade_web/live/invite_live.ex
+++ lib/arcade_web/live/invite_live.ex
@@ -5,20 +5,29 @@ defmodule ArcadeWeb.InviteLive do
 
   def mount(_params, _session, socket) do
     {:ok,
-      socket
-      |> assign_recipient()
-      |> assign_changeset()}
+     socket
+     |> assign_recipient()
+     |> assign_changeset()}
   end
-
 
   def assign_recipient(socket) do
     socket
     |> assign(:recipient, %Recipient{})
   end
 
-
   def assign_changeset(%{assigns: %{recipient: recipient}} = socket) do
     socket
     |> assign(:changeset, Invite.change_invitation(recipient))
+  end
+
+  def handle_event("validate", %{"recipient" => recipient_params}, %{assigns: %{recipient: recipient}} = socket) do
+    changeset =
+      recipient
+      |> Invite.change_invitation(recipient_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply,
+     socket
+     |> assign(:changeset, changeset)}
   end
 end
