8068ec4b85b1341c5746c5a4411175d8bc85a8b7
Håvar Stavseth <haavar.stavseth@gmail.com>
2021-12-18 21:37:34 +0100

    Added app code

---
+++ lib/arcade/invite.ex
@@ -0,0 +1,12 @@
+# defmodule Arcade.Invite do
+#   alias Arcade.Invite.Recipient
+
+#   def change_invitation(%Recipient{} = recipient, attrs \\ %{}) do
+#     Recipient.changeset(recipient, attrs)
+#   end
+
+
+#   def send_invite(recipient, attrs) do
+#     # send email to promo recipient
+#   end
+# end
